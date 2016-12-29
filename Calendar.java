f83505dd8ff4a64e1b9522bdd82061cd63013d71
vivaciouspupil <vivaciouspupil@gmail.com>
2016-12-29 19:42:31 +0800

    Version 1.0 of 2016_Calendar

---
+++ Calendar.java
@@ -0,0 +1,19 @@
+
+package Calendar;
+
+import java.util.Scanner;
+
+public class Calendar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Data data = new Data();
+		Scanner sc = new Scanner(System.in);
+		String s = "    ";
+		System.out.print("請輸入月:");
+		int month = sc.nextInt();
+		System.out.println(" 日  " + " 一  " + "二  " + "三  " + " 四  " + "五  " + " 六  ");
+		data.data(month, s);
+	}
+}
+
